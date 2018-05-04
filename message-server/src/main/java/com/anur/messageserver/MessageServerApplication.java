fa2f8fa230f49a060e2641faa50c77318a5c91bb
Anur <anurnomeru@163.com>
2018-05-04 18:16:19 +0800

    init

---
+++ message-server/src/main/java/com/anur/messageserver/MessageServerApplication.java
@@ -0,0 +1,12 @@
+package com.anur.messageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageServerApplication.class, args);
+    }
+}

67bb16469439c048be69c54dd9a1136c3892ee38
Anur <anurnomeru@163.com>
2018-05-04 18:26:35 +0800

    消息服务的创建以及maven的构建

--- message-server/src/main/java/com/anur/messageserver/MessageServerApplication.java
+++ message-server/src/main/java/com/anur/messageserver/MessageServerApplication.java
@@ -2,11 +2,22 @@ package com.anur.messageserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.Data;
+import lombok.extern.log4j.Log4j;
 
 @SpringBootApplication
 public class MessageServerApplication {
 
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setName("五菱荣光");
+        System.out.println(car.toString());
         SpringApplication.run(MessageServerApplication.class, args);
+    }
+
+    @Data
+    public static class Car {
+        String name;
     }
 }

70d89ba31b42015d03d46281406ce714f2fb22d5
Anur <anurnomeru@163.com>
2018-05-18 12:36:33 +0800

    version 1.0 构建完成

--- provider/src/main/java/com/anur/provider/cron/MsgConfirm.java
+++ provider/src/main/java/com/anur/provider/cron/MsgConfirm.java
@@ -2,6 +2,8 @@ package com.anur.provider.cron;
 
 import com.anur.provider.model.PrepareMsg;
 import com.anur.provider.service.PrepareMsgService;
+import com.github.pagehelper.PageHelper;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,7 @@ import java.util.List;
  * Created by Anur IjuoKaruKas on 2018/5/14
  */
 @Component
+@Log
 public class MsgConfirm {
 
     @Autowired
@@ -19,9 +22,10 @@ public class MsgConfirm {
 
     @Scheduled(cron = "*/1 * * * * *")
     public void checkPrepareMsg() {
+        PageHelper.startPage(1,10).setOrderBy("create_time desc");
         List<PrepareMsg> prepareMsgList = prepareMsgService.getUnConfirmList();
         if (prepareMsgList.size() > 0) {
-            System.out.println("消息重发中");
+            log.info("There is "+ prepareMsgList.size() +"to rePrepare");
         }
         for (PrepareMsg prepareMsg : prepareMsgList) {
             prepareMsgService.prepareMsg(prepareMsg);
